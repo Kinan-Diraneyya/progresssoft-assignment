b18dcabd6d446f943e37ed64d5fc478c1496d75c
Kinan <62595502+Kinan-Diraneyya@users.noreply.github.com>
2024-02-17 11:03:16 +0300

    Document the Deal model

--- src/main/java/bloomberg/deals/model/Deal.java
+++ src/main/java/bloomberg/deals/model/Deal.java
@@ -8,19 +8,40 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents an FX exchange transaction between two different currencies.
+ */
 @Data
 @Entity
 @Table(name = "deals")
 public class Deal {
+    /**
+     * The deal's unique ID.
+     * Note to reviewer: This ID is NOT auto generated, and that is intentional,
+     * as it was what I inferred from the below constraint:
+     * "System should not import same request twice."
+     */
     @Id
     @NotEmpty
     String id;
+    /**
+     * The ISO code of the currency from which the amount is being exchanged.
+     */
     @NotEmpty
     String fromCurrencyIsoCode;
+    /**
+     * The ISO code of the currency to exchanged to.
+     */
     @NotEmpty
     String toCurrencyIsoCode;
+    /**
+     * The timestamp at which the exchange was made
+     */
     @NotNull
     LocalDateTime timeStamp;
+    /**
+     * The amount being exchanged, in the fromCurrencyIsoCode currency.
+     */
     @NotNull
     @Positive
     double amountInFromCurrency;
